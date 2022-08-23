6e4bfe75993b7332b585f7f1a18786fdf361581d
Kym Natividad <kynatividad@archgroup.com>
2022-08-23 13:38:01 -0400

    Edited dependency and ran tf fmt

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,9 +1,9 @@
 resource "azurerm_resource_group" "my_rg" {
-  name      = "AMIS-AUTOMATION-CHALLENGE-kynatividad"
-  location  = "eastus2"
+  name     = "AMIS-AUTOMATION-CHALLENGE-kynatividad"
+  location = "eastus2"
 
   tags = {
-    AppDomain = "Automation Challenge 2.0"
+    AppDomain      = "Automation Challenge 2.0"
     AppDomainOwner = "Kym Natividad"
   }
 }
